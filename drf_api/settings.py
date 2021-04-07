3d34addd2f13cae542b5f27d0c1921d924332de7
hemanth-431 <bandlavhemanth@gmail.com>
2021-04-07 23:06:49 +0530

    first commit

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '8$g%tp=w--w*b1$y7(tv4wea-ovgmadx&(k-dz)j)%!i&hw2l)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','hemanthbandla.com']
 
 
 # Application definition
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -3,6 +3,9 @@ from django.urls import path,include
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
 from core.views import PostView,PostCreateView,TestView,PostListCreateView,loginPage,registerPage,home,Postregister
+from django.conf.urls import url
+from Tools.scripts import serve
+from drf_api import settings
 
 urlpatterns = [
      path('rest-auth/', include('rest_auth.urls')),
@@ -17,7 +20,9 @@ urlpatterns = [
     path('api/token/',obtain_auth_token,name='obtain-token'),
     path('login/',loginPage,name='login'),
     path('',registerPage),
-    path('home/',home)
+    path('home/',home),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 
 
 ]
